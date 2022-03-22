fe1b7cc098a458c0872a1d70befb541930ce4d61
ejgoodwin <ericagoodwin90@gmail.com>
2022-03-22 20:50:26 +0000

    update homepage sections

--- src/homePageData.js
+++ src/homePageData.js
@@ -1,6 +1,6 @@
 const testData = [
 	{
-		background: '#EDF8FD url("images/homepage-hero-bg.svg") no-repeat bottom',
+		background: '#EDF8FD',
 		color: '#26262a',
 		component: 'hero',
 		classes: '',
@@ -24,32 +24,51 @@ const testData = [
 		]
 	},
 	{
+		background: '#2D344D',
+		color: '#fff',
 		component: 'card',
-		classes: 'shadow',
+		classes: 'no-border',
 		items: [
 			{
-				heading: 'Card',
+				heading: 'Simple interface',
 				text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
 			},
 			{
-				heading: 'Card',
+				heading: 'Pre-made components',
 				text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
 			},
 			{
-				heading: 'Card',
+				heading: 'Copy and paste code',
 				text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
 			}
 		]
 	},
 	{
-		background: '#161e27',
+		component: 'cta-block',
+		classes: 'cta-block--left',
+		items: [
+			{
+				heading: "Heading about product",
+				text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+				image: 'https://placeimg.com/640/400/any'
+			}
+		]
+	},
+	{
+		background: 'linear-gradient(90deg, rgba(5,123,186,1) 40%, rgba(41,193,221,1) 100%)',
 		color: 'white',
 		component: 'text-block',
 		classes: 'text-block--center',
 		items: [
 			{
-				heading: "I'm a center aligned text block",
-				text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+				heading: "Create pages with an easy to use page builder",
+				buttons: [
+					{
+						label: 'Create new page',
+						link: '/page-builder',
+						classes: 'button--secondary'
+					}
+				]
 			}
 		]
 	}
