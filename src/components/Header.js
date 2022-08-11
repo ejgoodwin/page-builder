cbceff6d9e5d90546e73f98b49592920430c0d80
ejgoodwin <ericagoodwin90@gmail.com>
2022-08-11 12:47:31 +0100

    added NavLink and active styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../images/lockup.svg";
 
 const Header = () => {
+
+	const activeStyle = {
+		borderColor: "#29c1dd"
+	}
 	return (
 		<header className="header">
 			<Link className="header__lockup" to="/" >
 				<img src={logo} alt='Logo' />
 			</Link>
-			<div>
-				<Link className="header__link" to="/page-builder">Page builder</Link>
-				<Link className="header__link" to="/example-page">Example page</Link>
-			</div>
+			<nav>
+				<NavLink className="header__link" to="/" style={({ isActive }) => isActive ? activeStyle : undefined}>Home</NavLink>
+				<NavLink className="header__link" to="/page-builder" style={({ isActive }) => isActive ? activeStyle : undefined}>Page builder</NavLink>
+				<NavLink className="header__link" to="/example-page" style={({ isActive }) => isActive ? activeStyle : undefined}>Example page</NavLink>
+			</nav>
 		</header>
 	);
 }
